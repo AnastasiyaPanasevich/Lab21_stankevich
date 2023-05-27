aeebaf75a4d9b42034cf4c31f90c88a4d392a0e7
Anastasiya Panasevich <matildaweller66@gmail.com>
2023-05-27 08:35:26 +0300

    Polymorphism

--- prim1/Model/Model.cs
+++ prim1/Model/Model.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -34,28 +35,57 @@ namespace Lab21.Model
             Weight = sweet.Weight;
             sweetKind = sweet.sweetKind.ToString();
         }
+        public void print(Sweet sweet) 
+        {
+            Console.Write("\t{0}\t{1} byn\t{2}\t", sweet.Name.PadRight(15), sweet.Price.ToString().PadRight(5), sweet.sweetKind.PadRight(10));
+        }
     }
 
     public class Candy : Sweet
     {
         public bool IsSugarFree { get; set; }
+        public void print(Candy candy)
+        {
+            base.print(candy);
+            Console.Write(" {0}", candy.IsSugarFree ? "sugar-free".PadRight(15) : "full of sugar".PadRight(15));
+        }
     }
 
     public class Cookie : Sweet
     {
         public bool IsChocolate { get; set; }
+        public void print(Cookie cookie)
+        {
+            base.print(cookie);
+            Console.Write(" {0}", cookie.IsChocolate ? "chocolate".PadRight(15) : "non-chocolate".PadRight(15));
+        }
     }
     public class CookieFortune : Cookie
     {
         public bool HasAnswer { get; set; }
+        public void print(CookieFortune cookieF)
+        {
+            base.print(cookieF);
+            Console.Write(" {0}", cookieF.HasAnswer ? "the answer is yes".PadRight(15) : "the answer is no".PadRight(15));
+        }
     }
     public class Cheesecake : Sweet
     {
         public bool IsBacked { get; set; }
+        public void print(Cheesecake cheesecake)
+        {
+            base.print(cheesecake);
+            Console.Write(" {0}", cheesecake.IsBacked ? "was backed".PadRight(15) : "wasn't baked".PadRight(15));
+        }
     }
 
     public class Cupcake : Sweet
     {
         public bool IsFrosted { get; set; }
+        public void print(Cupcake cupcake)
+        {
+            base.print(cupcake);
+            Console.Write(" {0}", cupcake.IsFrosted ? "has frosting".PadRight(15) : "doesn't have frosting".PadRight(15));
+        }
     }
 }
